d58ad85773110f07e764b1f8cafd45204f5d3af0
Rodrigo Santos <epxter@gmail.com>
2022-08-26 10:52:20 -0300

    Create unit tests for GetMoveDetaisUseCase

--- app/src/test/java/com/chavesdev/mvvmsamplecode/movies/core/domain/usecases/GetMovieDetailsUseCaseTest.kt
+++ app/src/test/java/com/chavesdev/mvvmsamplecode/movies/core/domain/usecases/GetMovieDetailsUseCaseTest.kt
@@ -1,10 +1,11 @@
 package com.chavesdev.mvvmsamplecode.movies.core.domain.usecases
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
-import com.chavesdev.mvvmsamplecode.movies.core.domain.model.MoviesPage
+import com.chavesdev.mvvmsamplecode.movies.core.domain.model.MovieDetail
 import com.chavesdev.mvvmsamplecode.movies.core.domain.repo.MoviesRepository
 import com.chavesdev.mvvmsamplecode.movies.core.util.ResponseState
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -16,7 +17,7 @@ import java.io.IOException
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
-internal class GetPopularMoviesUseCaseTest {
+internal class GetMovieDetailsUseCaseTest {
 
     @get:Rule
     val testCoroutineRule = InstantTaskExecutorRule()
@@ -24,40 +25,37 @@ internal class GetPopularMoviesUseCaseTest {
     private val movieRepository: MoviesRepository = mockk(relaxed = true)
     private val httpException: HttpException = mockk(relaxed = true)
     private val ioException: IOException = mockk(relaxed = true)
+    private val movieDetail: MovieDetail = mockk()
 
-    private lateinit var getPopularMoviesUseCase: GetPopularMoviesUseCase
+    private lateinit var getMovieDetailsUseCase: GetMovieDetailsUseCase
 
     @Before
-    fun setUp() {
-        getPopularMoviesUseCase = GetPopularMoviesUseCase(movieRepository)
+    fun setup() {
+        getMovieDetailsUseCase = GetMovieDetailsUseCase(movieRepository)
     }
 
     @Test
     fun `check success state`() = runBlockingTest {
-        coEvery { movieRepository.listPopularMovies() }.returns(successresponse)
-        val state = getPopularMoviesUseCase.invoke()
-        assert(state is ResponseState.Success)
-        val moviesPage = state.data as MoviesPage
-        assert(moviesPage.page == 1)
-        assert(moviesPage.results.isEmpty())
-        assert(moviesPage.totalPages == 1)
+        every { movieDetail.adult } returns true
+        coEvery { movieRepository.getMovieDetails(any()) }.returns(movieDetail)
+        val state = getMovieDetailsUseCase.invoke(1)
+        assert(state is ResponseState.Success<MovieDetail>)
+        val movieDetail = state.data
+        assert(movieDetail?.adult ?: false)
     }
 
     @Test
     fun `check http exception`() = runBlockingTest {
-        coEvery { movieRepository.listPopularMovies() }.throws(httpException)
-        val state = getPopularMoviesUseCase.invoke()
+        coEvery { movieRepository.getMovieDetails(any()) }.throws(httpException)
+        val state = getMovieDetailsUseCase.invoke(1)
         assert(state is ResponseState.Error)
     }
 
     @Test
     fun `check io exception`() = runBlockingTest {
-        coEvery { movieRepository.listPopularMovies() }.throws(ioException)
-        val state = getPopularMoviesUseCase.invoke()
+        coEvery { movieRepository.getMovieDetails(any()) }.throws(ioException)
+        val state = getMovieDetailsUseCase.invoke(1)
         assert(state is ResponseState.Error)
     }
 
-    companion object{
-        val successresponse = MoviesPage(1, emptyList(), 1)
-    }
 }
